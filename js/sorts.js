c823dc368520b3773256192cff42ff3be5b68663
SKarachevcev <svyat.Karachevcev@gmail.com>
2015-12-02 00:48:53 +0200

    Update. New algorithms of sort.
    Added 3 new alhorithms, reworked buttons & textareas.

--- js/sorts.js
+++ js/sorts.js
@@ -20,7 +20,9 @@ var Arr= new Array();
 	
 		function rfarr()
 		{
-			var Ar=$("#numbs").val();	
+			i=0;
+			
+			var Ar=$("#numbs").val();
 			var Xar=Ar.split(" ");
 			var xi=i;
 			for(;xi<Xar.length-1;xi++,i++)
@@ -30,73 +32,189 @@ var Arr= new Array();
 		}
 	
      	function add_new_number()
-     	{
+     	{		
+			if(Number.value)
+			{
 			Arr[i]=parseFloat(Number.value);
 			Number.value="";
             textarea1.value+=Arr[i]+ " ";
 		    i++;
+			}
 		}
 
+function cta2()
+{
+	textarea1.value="";
+	setTimeout(showarray, 500);
+}
+		
 		function showarray()
 		{
 			var j;
-            textarea1.value="";
 			for(j=0;j<i;j++)
 			{
-				if(Arr[j] == '' || isNaN(Arr[j])) Arr[j]=0;
 				textarea1.value += Arr[j] + " ";
 			}
 			
 		}
-		var output;
-		function readLines(file)
-		{
-			
-			
-			 output = false;
-			$.ajax(
-			{
-				url: file,
-				success: function(data)
-				{
-						output = data.split(" ");
-						console.log(output);        // для проверки выведем в консоль, что получилось
-				}
-			});
-			return output;
-		}
+		
 
-var Arr2=new Array();
 
+var Arr3=new Array();
+var st,et;
 		function buble()
 		{
-                for(var x=0;x<i;x++)
-                {                	Arr2[x]=Arr[x];                }
 
+st=parseFloat(Date.now());
             for(var k=0;k<i;k++)
             {
-            var Arr_min=Arr2[k];
+            var Arr_min=Arr3[k];
             	for(var l=0;l<i;l++)
             	{
-            		if(Arr2[l]>Arr_min)
-            		{            			var tmp=Arr2[k];
-            			Arr_min=Arr2[l];
-            			Arr2[k]=Arr_min;
-            			Arr2[l]=tmp;
+            		if(Arr3[l]>Arr_min)
+            		{            			var tmp=Arr3[k];
+            			Arr_min=Arr3[l];
+            			Arr3[k]=Arr_min;
+            			Arr3[l]=tmp;
             		}
             	}
             }
-
+et=parseFloat(Date.now());
 		}
 
+		
 
+	
+function shake()
+{
+
+
+st=parseFloat(Date.now());
+				var ba=0;
+				var ea=Arr3.length-1;
+		for (var x=0; x < Arr3.length/2; x++)
+		{	
+		ba=0;
+		ea=Arr3.length-1;
+	    do
+	    {
+			if (Arr3[ba + 1] < Arr3[ba])
+			{
+				var tmp=Arr3[ba];
+				Arr3[ba]=Arr3[ba+1];
+				Arr3[ba+1]=tmp;
+			}
+			ba++;
+
+			if (Arr3[ea-1] >  Arr3[ea])
+			{
+				var tmp=Arr3[ea];
+				Arr3[ea]=Arr3[ea-1];
+				Arr3[ea-1]=tmp;
+			}
+			ea--;
+		}while(ba<=ea);
+	}		
+	et=parseFloat(Date.now());
+}
+
+function inps()
+{
+
+		
+	st=parseFloat(Date.now());
+	for(var j = 1; j < Arr3.length; j++)  
+    {
+		var key = Arr3[j];
+		var x = j - 1;
+		while(x >= 0 && Arr3[x] > key)
+		{
+			Arr3[x+1] = Arr3[x];
+			x--;
+		}
+
+		Arr3[x+1] = key;
+	}
+	et=parseFloat(Date.now());
+}
+
+		
+function sel()
+{
+	
+	st=parseFloat(Date.now());
+    for (var idx_i = 0; idx_i < Arr3.length-1; idx_i++)
+    {
+        var min_idx = idx_i;
+        for (var idx_j = idx_i + 1; idx_j < Arr.length; idx_j++)
+        {
+            if (Arr3[idx_j] < Arr3[min_idx])
+            {
+                min_idx = idx_j;
+            }
+        }
+
+        if (min_idx != idx_i)
+        {
+				var tmp=Arr3[idx_i];
+				Arr3[idx_i]=Arr3[min_idx];
+				Arr3[min_idx]=tmp;
+        }
+    }
+	et=parseFloat(Date.now());
+}
+
+
+function checkInput(ob) {
+  var invalidChars = /[^0-9, ,e]/gi
+  if(invalidChars.test(ob.value)) {
+            ob.value = ob.value.replace(invalidChars,"");
+      }
+}
+
+function cta()
+{
+	textarea2.value="";
+	setTimeout(showsortarray, 500);
+}
 
 		function showsortarray()
 		{
+			for(var x=0;x<Arr.length;x++)
+			{
+				Arr3[x]=Arr[x];
+			}
+			var inp = document.getElementsByName('r');
+			if(inp[0].checked || inp[1].checked || inp[2].checked || inp[3].checked)
+			{
+			textarea2.value="";
+			if (inp[0].checked)
+			{
+				buble();
+			}
+			if (inp[1].checked)
+			{
+				inps();
+			}
+			if (inp[2].checked)
+			{
+				sel();
+			}
+			if (inp[3].checked)
+			{
+				shake();
+			}			
+			
+			var Timer=parseFloat(et-st);
 			var j;
-            textarea2.value="";
 			for(j=0;j<i;j++)
 			{
-				textarea2.value+=Arr2[j]+ " ";
+				textarea2.value+=Arr3[j]+ " ";
 		    }
+			document.getElementById('TNNN').value=Timer/100+"sec";
+			}
+			else
+			{
+				alert("No one type selected!!! Choose one & try again.");
+			}
 		}
